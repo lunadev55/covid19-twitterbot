4221c69fcbb335958f19079e1fbfe6a59d31caef
mangueBit <alm3@cin.ufpe.br>
2020-03-30 02:25:18 -0300

    formats date, scrap main table, pandas to structure data into csv

--- covid19.py
+++ covid19.py
@@ -20,9 +20,7 @@ import tweepy as tp
 import time
 import os
 
-# credentials to login to twitter api
-# you should create a twitter developer account to have access ...
-# ... to your keys and them fill them in down below
+#credentials to login to twitter api
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
@@ -32,6 +30,22 @@ access_secret = ''
 auth = tp.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tp.API(auth)
+
+# date and time to Brazilian format
+def formatDate(hour):
+    day = hour[8:10]
+    mon = hour[5:7]
+    year = hour[:4]
+    full_date = day + '/' + mon + '/' + year
+
+    hours = hour[11:13]
+    minutes = hour[14:16]
+    full_hours = hours + ':' + minutes
+
+    full_string = ('Data: ' + full_date + '\n' + 
+                    'Horario: ' + full_hours)
+
+    return full_string
 
 while (True):    
     url = 'https://www.worldometers.info/coronavirus/'
@@ -45,21 +59,73 @@ while (True):
 
     # get the 3 maincounter-number div class in which have the content for
     # cases, deaths and recovered
-    elem = driver.find_elements_by_xpath("//div[@class='maincounter-number']//span")
+    main = driver.find_elements_by_xpath("//div[@class='maincounter-number']//span")
 
-    cases = elem[0].get_attribute('innerHTML')
-    deaths = elem[1].get_attribute('innerHTML')
-    recoveres = elem[2].get_attribute('innerHTML')
+    cases = main[0].get_attribute('innerHTML')
+    deaths = main[1].get_attribute('innerHTML')
+    recoveres = main[2].get_attribute('innerHTML')
+   
+    element = driver.find_element_by_id('main_table_countries_today')
+    html_content = element.get_attribute('outerHTML')   
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find(name='table')
+
+    df_full = pd.read_html(str(table))[0]
+
+    df = df_full.to_csv(index=False)
+
+    # print(df)
+    f = open('covid_table.txt', 'w+')
+    f.write(df)
+
+    # IMPORTANT
+
+    # estruturar conteudo em data fame - PANDAS
+    # df_full = pd.read_html(str(table))[0]
+    # df = df_full[['Country,Other', 'TotalCases']]
+    # df.columns = ['Pais', 'Total de Casos']
+    # print(df_full)
+
+    # f = open('covid_table.txt', 'w+')
+    # f.write(df_full.to_string())
+
+    # IMPORTANT
 
     driver.quit()
 
-    api.update_status('CONTEUDO ATUALIZADO SOBRE O COVID-19\n\n' +
-                        'DATA: ' + str(datetime.now()) + '\n\n' +
+
+    # SAMPLE:
+    # CONTEUDO ATUALIZADO SOBRE O COVID-19
+
+    # Data: 30/03/2020
+    # Horario: 01:00
+
+    # CASOS NO BRASIL: 
+    # MORTES NO BRASIL:
+    # RECUPERADOS:
+
+    # CASOS NO MUNDO: 722,196
+    # MORTES: 33,976
+    # RECUPERADOS: 151,766
+
+    
+
+    date = formatDate(str(datetime.now()))
+
+    post = ('CONTEUDO ATUALIZADO SOBRE O COVID-19\n\n' +
+                        date + '\n\n' +
                         'CASOS NO MUNDO: ' + cases + '\n' +
                         'MORTES: ' + deaths + '\n' +
                         'RECUPERADOS: ' + recoveres + '\n')
 
-    time.sleep(20)
+    # tweets string
+    # api.update_status(post)
+
+    # print (post)
+
+
+    time.sleep(15)
 
 
 
