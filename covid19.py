8065d7db4d4733e9144e3edb482b3323fe0442ab
mangueBit <alm3@cin.ufpe.br>
2020-03-31 19:01:54 -0300

    updates

--- covid19.py
+++ covid19.py
@@ -19,6 +19,8 @@ from datetime import datetime
 import tweepy as tp
 import time
 import os
+import csv
+import random
 
 #credentials to login to twitter api
 consumer_key = ''
@@ -31,101 +33,97 @@ auth = tp.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tp.API(auth)
 
-# date and time to Brazilian format
+# date to Brazilian format
 def formatDate(hour):
     day = hour[8:10]
     mon = hour[5:7]
     year = hour[:4]
     full_date = day + '/' + mon + '/' + year
-
-    hours = hour[11:13]
-    minutes = hour[14:16]
-    full_hours = hours + ':' + minutes
+    
+    hours = hour[11:16]
 
     full_string = ('Data: ' + full_date + '\n' + 
-                    'Horario: ' + full_hours)
+                    'Horario: ' + hours)
 
     return full_string
 
+
+# infinite loop to keep posting tweets
 while (True):    
+    # web page to get data from
     url = 'https://www.worldometers.info/coronavirus/'
 
     option = Options()
     option.headless = True
+    
+    # firefox to load web page, if 'options=option' deleted ...
+    # ... browser opens and you can see the process going on
     driver = webdriver.Firefox(options=option)
     driver.get(url)
 
-    time.sleep(10)
+    time.sleep(10) # wait until web page is fully loaded
 
-    # get the 3 maincounter-number div class in which have the content for
-    # cases, deaths and recovered
+    # get main element with worldwide info (cases, deaths and recovered)
     main = driver.find_elements_by_xpath("//div[@class='maincounter-number']//span")
 
     cases = main[0].get_attribute('innerHTML')
     deaths = main[1].get_attribute('innerHTML')
     recoveres = main[2].get_attribute('innerHTML')
    
+    # get main content table with worldwide info
     element = driver.find_element_by_id('main_table_countries_today')
     html_content = element.get_attribute('outerHTML')   
 
+    # beautifulsoup to parse html and get table element
     soup = BeautifulSoup(html_content, 'html.parser')
     table = soup.find(name='table')
 
+    # pandas reads full html table and converts to csv
     df_full = pd.read_html(str(table))[0]
-
     df = df_full.to_csv(index=False)
 
-    # print(df)
-    f = open('covid_table.txt', 'w+')
+    f = open('covid_tb2.csv', 'w+')
     f.write(df)
 
-    # IMPORTANT
+    brazil = ''
 
-    # estruturar conteudo em data fame - PANDAS
-    # df_full = pd.read_html(str(table))[0]
-    # df = df_full[['Country,Other', 'TotalCases']]
-    # df.columns = ['Pais', 'Total de Casos']
-    # print(df_full)
-
-    # f = open('covid_table.txt', 'w+')
-    # f.write(df_full.to_string())
-
-    # IMPORTANT
-
-    driver.quit()
-
-
-    # SAMPLE:
-    # CONTEUDO ATUALIZADO SOBRE O COVID-19
-
-    # Data: 30/03/2020
-    # Horario: 01:00
-
-    # CASOS NO BRASIL: 
-    # MORTES NO BRASIL:
-    # RECUPERADOS:
-
-    # CASOS NO MUNDO: 722,196
-    # MORTES: 33,976
-    # RECUPERADOS: 151,766
-
-    
+    # opens csv, iterates through the file in search of Brazil ...
+    # ... and builds string to tweet
+    file2 = csv.reader(open('covid_tb2.csv'), delimiter=',')
+    x = 0
+    flag = False
+    for line in file2:
+        for i in range(len(line)):
+            if (line[i] == 'Brazil'):
+                brazil = ('CASOS NO BRASIL: ' + line[1] + '\n' +
+                            'MORTES: ' + line[3] + '\n' +
+                            'CURADOS: ' + line[5] + '\n\n')
+                flag = True
+                break
+        if (flag):
+                break
+        
+    driver.quit()    
 
     date = formatDate(str(datetime.now()))
 
+    # concat final string to be posted
     post = ('CONTEUDO ATUALIZADO SOBRE O COVID-19\n\n' +
                         date + '\n\n' +
+                        brazil +
                         'CASOS NO MUNDO: ' + cases + '\n' +
                         'MORTES: ' + deaths + '\n' +
-                        'RECUPERADOS: ' + recoveres + '\n')
+                        'CURADOS: ' + recoveres)
 
     # tweets string
-    # api.update_status(post)
+    api.update_status(post)
 
-    # print (post)
+    # generates random number in range (120, 300) representing seconds
+    # tweets have an interval of 2 to 3 minutes
+    t = random.randrange(120, 300)
 
-
-    time.sleep(15)
+    # sleep the random generate seconds until next post
+    time.sleep(t)
 
 
 
